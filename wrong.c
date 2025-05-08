827d5b9a4e43b445f044c56c6b84b7251a9040da
kevin9507082nd <kevin9507082nd@gmail.com>
2025-05-08 10:19:04 +0800

    Update wrong.c

--- wrong.c
+++ wrong.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
-#include<string.h>
+#include<stdlib.h>
+#include<math.h>
 
 int main(){
-    int T;
-    scanf("%d",&T);
-    while(T--){
-        int J;
-        int print[256]={0};
-        scanf("%d",&J);
-        
-        for(int i=0;i<J;i++){
-            int val;
-            char ch;
-            scanf(" %c %d",&ch,&val);
-            getchar();
-            print[(unsigned char)ch]=val;
-            
-        }
-        getchar();
-        int cent;
-        scanf("%d",&cent);
-        int count;
-        scanf("%d",&count);
-        char line[10001];
-        long long sum=0;
-        for(int i=0;i<count;i++){
-            fgets(line,sizeof(line),stdin);
-            for(int j=0;line[j];j++){
-                sum+=print[(unsigned char)line[j]];
-            }
-        }
-        printf("%lld.%02lld$\n",sum/100,sum%100);
-        if(sum>cent){
-            printf("Yes");
-        }else{
-            printf("No");
+    int m,n;
+    int num[1000]={0};
+    int count[1000]={0};
+    for(int i=1;i<1000;i++){
+        for(int j=1;j<=i;j++)
+        if(i%j==0){
+            num[i]++;
         }
     }
+    int k=0;
+    for(int i=0;i<1000;i++){
+        num[1]=2;
+        
+        if(num[i]==2){
+            
+            count[k]=i;
+            k++;
+        }
+    
+    }
+    
+    while(scanf("%d %d",&n,&m)==2){
+        printf("%d %d:",n,m);
+        int i=0;
+        for(;i<=n;i++){
+            if(count[i]>n){
+                break;
+            }
+        }
+        i=i-n;
+        for(int j=i;count[j]<n;j++){
+            printf(" %d",count[j]);
+        }
+        printf("\n\n");
+    }
+    return 0;
 }
