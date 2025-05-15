be4e363da136e706d233398f526ee37a473469d0
kevin9507082nd <kevin9507082nd@gmail.com>
2025-05-15 10:26:59 +0800

    Update wrong.c

--- wrong.c
+++ wrong.c
@@ -1,42 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    int m,n;
-    int num[1000]={0};
-    int count[1000]={0};
-    for(int i=1;i<1000;i++){
-        for(int j=1;j<=i;j++)
-        if(i%j==0){
-            num[i]++;
-        }
-    }
-    int k=0;
-    for(int i=0;i<1000;i++){
-        num[1]=2;
+int cool_word(char *word)
+{
+    int freq[26] = {0};
+    int used_freq[35]={0};
+    int letter = 0;
+    for (int i = 0; word[i]; i++)
+    {
+        int idk = word[i] - 'a';
         
-        if(num[i]==2){
-            
-            count[k]=i;
-            k++;
+        if (freq[idk]==0)
+        {
+            letter++;
         }
-    
+        freq[idk]++;
     }
-    
-    while(scanf("%d %d",&n,&m)==2){
-        printf("%d %d:",n,m);
-        int i=0;
-        for(;i<=n;i++){
-            if(count[i]>n){
-                break;
+
+    if (letter < 2)
+        return 0;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i]>0)
+        {
+            if (used_freq[freq[i]])
+                return 0;
+            used_freq[freq[i]] = 1;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    char word[40];
+    int count = 1;
+    int word_s = 0;
+    char recode[40];
+
+    while (scanf("%d", &n) != EOF)
+    {
+        word_s = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%s", word);
+            if (cool_word(word))
+            {
+                word_s++;
+                recode[word_s] = word[word_s];
             }
         }
-        i=i-n;
-        for(int j=i;count[j]<n;j++){
-            printf(" %d",count[j]);
+        printf("Case %d: %d\n", count++, word_s);
+        for (int i = 0; i < word_s; i++)
+        {
+            printf("%c", recode[i]);
         }
-        printf("\n\n");
+        printf("\n");
     }
     return 0;
 }
